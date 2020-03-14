189cd372b15293e2c04e9ba36abe490dbf519fce
soul <ykrainec1991@gmail.com>
2020-03-14 09:42:35 +0100

    create new method

--- src/main/java/pl/coderslab/dao/BookDao.java
+++ src/main/java/pl/coderslab/dao/BookDao.java
@@ -51,6 +51,9 @@ public class BookDao {
      *
      * @return
      */
+    public void newMetoda(){
+
+    }
     public List<Book> findAll() {
         List<Book> bookList = new ArrayList<>();
         try (Connection connection = DbUtil.getConnection();
