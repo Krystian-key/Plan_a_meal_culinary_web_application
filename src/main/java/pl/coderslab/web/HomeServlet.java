e41aae10a6f0a469dc4a625c3b664e92c2e16a27
Krystian <you@example.com>
2020-03-14 15:54:25 +0100

    Task completed, without changes.

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,3 +1,4 @@
+
 package pl.coderslab.web;
 
 import pl.coderslab.dao.BookDao;
@@ -11,17 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+
 /**
  * Do not change servlet address !!!
  */
+
 @WebServlet("")
 public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookDao bookDao = new BookDao();
-        List<Book> books = bookDao.findAll();
-        System.out.println(books);
+       // BookDao bookDao = new BookDao();
+   //     List<Book> books = bookDao.findAll();
+     //  System.out.println(books);
 
         getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
     }
 }
+
