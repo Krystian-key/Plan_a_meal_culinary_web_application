c1d50338abacd76202759e263a861ff05198423e
soul <ykrainec1991@gmail.com>
2020-03-14 20:00:53 +0100

    Added LoginServlet.java modified header.jsp

---
+++ src/main/java/pl/coderslab/web/LoginServlet.java
@@ -0,0 +1,21 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "login", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.html").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
