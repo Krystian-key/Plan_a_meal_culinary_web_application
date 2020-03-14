7268ed27c5539b85f6dea5ea31c65386812426e3
soul <ykrainec1991@gmail.com>
2020-03-14 12:59:41 +0100

    Create new classes: Plan and PlanDao

---
+++ src/main/java/pl/coderslab/dao/PlanDao.java
@@ -0,0 +1,161 @@
+package pl.coderslab.dao;
+
+import pl.coderslab.exception.NotFoundException;
+import pl.coderslab.model.Book;
+import pl.coderslab.model.Plan;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanDao {
+    // ZAPYTANIA SQL
+    private static final String CREATE_PLAN_QUERY = "INSERT INTO plan(name,description,created, adminID) VALUES (?,?,?,?);";
+    private static final String DELETE_PLAN_QUERY = "DELETE FROM plan where id = ?;";
+    private static final String FIND_ALL_PLAN_QUERY = "SELECT * FROM plan;";
+    private static final String READ_PLAN_QUERY = "SELECT * from plan where id = ?;";
+    private static final String UPDATE_PLAN_QUERY = "UPDATE	plan SET name = ? , description = ?, created = ?, admin_id = ? WHERE	id = ?;";
+
+    /**
+     * Get book by id
+     *
+     * @param planID
+     * @return
+     */
+    public Plan readPlan(Integer planID) {
+        Plan plan = new Plan();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(READ_PLAN_QUERY)
+        ) {
+            statement.setInt(1, planID);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    plan.setId(resultSet.getInt("id"));
+                    plan.setName(resultSet.getString("name"));
+                    plan.setDescription(resultSet.getString("description"));
+                    plan.setCreated(resultSet.getTimestamp("created").toLocalDateTime());
+                    plan.setAdminId(resultSet.getInt("adminID"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return plan;
+
+    }
+
+    /**
+     * Return all plans
+     *
+     * @return
+     */
+
+    public List<Plan> findAllPlan() {
+        List<Plan> plansList = new ArrayList<>();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL_PLAN_QUERY);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Plan planToADD = new Plan();
+                planToADD.setId(resultSet.getInt("id"));
+                planToADD.setName(resultSet.getString("name"));
+                planToADD.setDescription(resultSet.getString("description"));
+                planToADD.setCreated(resultSet.getTimestamp("created").toLocalDateTime());
+                planToADD.setAdminId(resultSet.getInt("adminID"));
+                plansList.add(planToADD);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return plansList;
+
+    }
+
+    /**
+     * Create plan
+     *
+     * @param plan
+     * @return
+     */
+    public Plan createPlan(Plan plan) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement insertStm = connection.prepareStatement(CREATE_PLAN_QUERY,
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            insertStm.setString(1, plan.getName());
+            insertStm.setString(2, plan.getDescription());
+            insertStm.setString(3, plan.getCreated().toString());
+            insertStm.setInt(4, plan.getAdminId());
+
+            int result = insertStm.executeUpdate();
+
+            if (result != 1) {
+                throw new RuntimeException("Execute update returned " + result);
+            }
+
+            try (ResultSet generatedKeys = insertStm.getGeneratedKeys()) {
+                if (generatedKeys.first()) {
+                    plan.setId(generatedKeys.getInt(1));
+                    return plan;
+                } else {
+                    throw new RuntimeException("Generated key was not found");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Remove book by id
+     *
+     * @param planID
+     */
+    public void deletePlan(Integer planID) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_PLAN_QUERY)) {
+            statement.setInt(1, planID);
+            statement.executeUpdate();
+
+            boolean deleted = statement.execute();
+            if (!deleted) {
+                throw new NotFoundException("Product not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Update book
+     *
+     * @param plan
+     */
+    public void updatePlan(Plan plan) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(UPDATE_PLAN_QUERY)) {
+            statement.setInt(4, plan.getId());
+            statement.setString(1, plan.getName());
+            statement.setString(2, plan.getDescription());
+            statement.setString(3, plan.getCreated().toString());
+            statement.setString(4, plan.getDescription());
+
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
