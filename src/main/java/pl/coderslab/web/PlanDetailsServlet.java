c71d15bb60c09ee8e353664096dbb532b67c75db
soul <ykrainec1991@gmail.com>
2020-03-21 11:38:53 +0100

    Fixed option details of plan: now if plan don't have any details then will show only name and description

--- src/main/java/pl/coderslab/web/PlanDetailsServlet.java
+++ src/main/java/pl/coderslab/web/PlanDetailsServlet.java
@@ -1,9 +1,11 @@
 
 package pl.coderslab.web;
 
+import pl.coderslab.dao.PlanDao;
 import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.dao.RecipePlanDao;
 import pl.coderslab.model.DisplayPlan;
+import pl.coderslab.model.Plan;
 import pl.coderslab.model.Recipe;
 import pl.coderslab.model.RecipePlan;
 
@@ -22,8 +24,13 @@ public class PlanDetailsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
         DisplayPlan displayPlan = new RecipePlanDao().detailsPlan(id);
-        request.setAttribute("recipePlanDao", displayPlan);
-        request.setAttribute("plan", displayPlan.getPlan());
+        if(displayPlan.getPlan().getName() != null) {
+            request.setAttribute("recipePlanDao", displayPlan);
+            request.setAttribute("plan", displayPlan.getPlan());
+        }else {
+            Plan recipePlan = new PlanDao().readPlan(id);
+            request.setAttribute("plan", recipePlan);
+        }
         request.getRequestDispatcher("/app-details-schedules.jsp").forward(request, response);
 
     }
