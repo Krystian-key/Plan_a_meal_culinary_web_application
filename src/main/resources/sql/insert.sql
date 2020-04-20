56e4245ea863edee78d3d8de48f9c7b506aaf3dd
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-04-20 21:58:52 +0100

    Refactoring code.

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -36,3 +36,13 @@ INSERT INTO `recipe_plan` (`id`, `recipe_id`,  `meal_name`, `display_order`, `da
 (null, 2,  'Śniadanie', 1, 1, 6),
 (null, 1,  'Kolacja', 2, 1, 6),
 (null, 3,  'Śniadanie', 1, 2, 6);
+
+INSERT INTO `pages` (`id`, `title`, `description`, `slug`) VALUES
+(null, 'Aplikacja Zaplanuj Jedzonko to nie tylko różnorodne i ciekawe przepisy, ale również codzienny niezbędnik!.', 'Każdy przepis jest opatrzony atrakcyjnymi dla oka obrazami oraz poradami dotyczącymi przygotowania dań. Jako,
+że aplikacja została zainspirowana przepisami Pani Marii Iksińskiej możecie liczyć na autorskie i nowoczesne przepisy.
+Aplikacja zainspirowana książką która jest aktulanym bestsellerem na rynku Polsce.
+', 'title1'),
+(null, 'Napraw swoje codzienne nawyki żywieniowe i skorzystaj z przepisów Marii Iksińskiej!', 'Zaplanuj Jedzonko to świetne narzędzie dla osób, które uwielbiają w aplikacjach funkcje monitorowania swojego jadłospisu.
+Dzięki naszej aplikacji będziecie mogli samodzielnie dodać zbilansowane przepisy i posmakować zdrowie na nowo.', 'Title2'),
+(null, 'Dzięki naszej aplikacji z łatwością przygotujesz posiłek dla całej rodziny!', 'Oprócz tego, użytkownik dowiaduje się, czy wykonanie dania jest proste,dla ilu osób
+przewidziane są zaprezentowane ilości składników, a także czas, który będziemy musiał na to poświęcić.', 'Title3');
